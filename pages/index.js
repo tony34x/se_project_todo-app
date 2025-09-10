2d4a1bae40b7a0cae359a84c1abe2b5fc0fe3c5d
tony34X <bookerjermiah31@gmail.com>
2025-09-10 12:11:40 -0500

    Add constants module for initialTodos and validationConfig; remove unused script tag from index.html

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,9 @@ const validationConfig = {
   inputErrorClass: "popup__input_type_error",
   inactiveButtonClass: "button_disabled",
 };
+import { initialTodos, validationConfig } from "../utils/constants.js";
+
+
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
