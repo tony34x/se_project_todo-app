1cd890df0dc7ad867d139f6bd4e4614af120082e
tony34X <bookerjermiah31@gmail.com>
2025-09-24 16:07:21 -0500

    Refactor FormValidator class to improve input validation and event handling; add launch configuration for debugging

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,12 +1,48 @@
 class FormValidator {
   constructor(settings, formElement) {
-    this._settings = settings;
-    this._formElement = document.querySelector(formElement);
-    console.log(settings);
-    console.log(formElement);
+    this._inputSelector = settings.inputSelectorSelector;
+    this._FormSelector = settings.formSelector;
+    this._submitButtonSelector = settings.submitebuttontSelector;
+    this._errorClass = settings.errorClass;
+    this._inputErrorClass = settings._inputErrorClass;
+    this._inactiveButtonClass = settings._inactiveButtonClass;
+    this._formElement = formElement;
   }
-enableValidation() {
-}
+  cheakInputValidity(inputList) {
+    // ToDO -implement this method
+    // copy body of existing function
+
+  }
+
+  _setEventListeners () {
+    const inputList = Array.from(
+      this._formElement.querySelectorAll(settings.inputSelector)
+    );
+    const buttonElement = formElement.querySelector(
+      settings.submitButtonSelector
+    );
+
+    togglebuttonState(inputList, buttonElement, settings)
+
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._cheakInputValidity(inputElement);
+        togglebuttonState(inputList, buttonElement, settings);
+      });
+    });
+
+  }
+
+
+
+
+  enableValidation() {
+    const formElement = document.querySelector(this._formSelector);
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
 }
 
 export default formValidator;
