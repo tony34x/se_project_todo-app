b6c2d56ff6c6120212e5aa2ee5daa00f5f8ade1e
tony34x <bookerjermiah31@gmail.com>
2025-10-11 20:34:50 -0500

    Fix constructor parameter assignment and improve formatting in FormValidator class

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,40 +1,37 @@
 class FormValidator {
-  constructor(settings, formElement ) {
-    this._inputSelector = settings.inputSelectorSelector;
+  constructor(settings, formElement) {
+    this._inputSelector = settings.inputSelector;
     this._FormSelector = settings.formSelector;
-    this._submitButtonSelector = settings.submitButtonSelector;
+    this._submitButtonSelector = validationConfig.submitButtonSelector;
     this._errorClass = settings.errorClass;
     this._inputErrorClass = settings._inputErrorClass;
     this._inactiveButtonClass = settings._inactiveButtonClass;
     this._FormElement = formElement;
     this._settings = settings;
-  };
+    this._togglebuttonState = this._togglebuttonState;
+  }
   checkInputValidity(formElement, inputElement, settings) {
     // ToDO -implement this method
     // copy body of existing function
-  };
+  }
   _setEventListeners() {
     this._inputList = Array.from(
       this._FormElement.querySelectorAll(this._inputSelector)
     );
-    debugger;
     const buttonElement = this._FormElement.querySelector(
       this._settings.submitButtonSelector
     );
- 
-
     this._togglebuttonState(this._inputList, buttonElement, this._settings);
-
     this._inputList.forEach((inputElement) => {
       inputElement.addEventListener("input", () => {
         this._checkInputValidity(inputElement);
         this._togglebuttonState(inputList, buttonElement, this._settings);
       });
     });
-  };
+  }
 
   enableValidation() {
-  const formElement = document.querySelector(this._FormSelector);
+    const formElement = document.querySelector(this._FormSelector);
     formElement.addEventListener("submit", (evt) => {
       evt.preventDefault();
     });
