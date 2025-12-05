b4c36a1d4d4d59ed7e710b0a093dfe9db1c09f0f
tony34x <bookerjermiah31@gmail.com>
2025-12-05 11:33:46 -0600

    Fix popup element selection in Popup constructor

--- components/popup.js
+++ components/popup.js
@@ -2,7 +2,7 @@ class Popup {
   constructor(popupSelector) {
     // FIX: Use the popupSelector variable to select the element.
     // Ensure that the value passed to popupSelector is a valid CSS selector (e.g., ".popup_type_edit" or "#my-popup").
-    this._popupElement = document.querySelector(popupSelector); // Changed from "popupSelector" to popupSelector
+    this._popupElement = document.querySelector(popupSelector);
     this._popupcloseBtn = this._popupElement.querySelector(".popup__close");
   }
 
