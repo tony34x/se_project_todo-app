17bb6c1f427a5eed4062d466fb680d646fc1d829
tony34x <bookerjermiah31@gmail.com>
2025-12-09 16:37:17 -0600

    Refactor Popup class to use class fields for event handler and improve method structure

--- components/popup.js
+++ components/popup.js
@@ -1,34 +1,31 @@
 class Popup {
   constructor(popupSelector) {
-
     this._popupElement = document.querySelector(popupSelector);
     this._popupcloseBtn = this._popupElement.querySelector(".popup__close");
   }
 
- // ... rest of your class code
+  // ... rest of your class code
+  // inside class body
+  _handleEscapeClose = (event) => {
+    if (event.key === "Escape") this.close();
+  };
 
- _handleEscapeClose(event) {
-    if (event.key === "Escape") {
-      this.close();
-    }
- }
   open() {
     this._popupElement.classList.add("popup_visible");
     document.addEventListener("keydown", this._handleEscapeClose);
   }
 
-close() {
-  this._popupElement.classList.remove("popup_visible");
-  // remove the escape-key listener that was added in open()
-  document.removeEventListener("keydown", this._handleEscapeClose);
-  console.log("Closing method called");
-}
+  close() {
+    this._popupElement.classList.remove("popup_visible");
+    // remove the escape-key listener that was added in open()
+    document.removeEventListener("keydown", this._handleEscapeClose);
+    console.log("Closing method called");
+  }
 
   setEventListeners() {
-    
     this._popupcloseBtn.addEventListener("click", () => {
-    // FIX: Call the close method on the Popup instance, not the DOM element.
-    this.close(); 
+      // FIX: Call the close method on the Popup instance, not the DOM element.
+      this.close();
     });
   }
 }
