b721322f1bcb327ddc8236b9541d5d4fa3832a52
tony34x <bookerjermiah31@gmail.com>
2025-12-16 16:46:58 -0600

    Remove unnecessary variable declaration for todoDeleteBtn in Todo class

--- components/Todo.js
+++ components/Todo.js
@@ -18,7 +18,7 @@ class Todo {
     const todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
     const todoLabel = this._todoElement.querySelector(".todo__label");
     const todoDate = this._todoElement.querySelector(".todo__date");
-    const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+  
 
     todoNameEl.textContent = this._data.name;
     const dueDate = new Date(this._data.date);
