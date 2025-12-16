457925bcab7e16629c41357b8d6ff84054ce95d1
tony34x <bookerjermiah31@gmail.com>
2025-12-16 16:26:22 -0600

    Remove unused _generateCheckboxEl method from Todo class

--- components/Todo.js
+++ components/Todo.js
@@ -9,14 +9,7 @@ class Todo {
       this._todoElement.remove();
     });
   }
-  _generateCheckboxEl() {
-    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
-    this._todoLabel = this._todoElement.querySelector(".todo__label");
-    this._todoCheckboxEl.checked = this._data.completed;
-    this._todoCheckboxEl.id = `todo-${this._data.id}`;
-    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
-  }
-
+  
   getView() {
     this._todoElement = this._templateElement.content
       .querySelector(".todo")
